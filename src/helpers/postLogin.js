e1456f64973da3dda2ddc0a29df95c2a770153a3
Enzo Gennari <enzo.gennari02@gmail.com>
2023-07-20 17:33:42 -0300

    Requisito 4

--- src/helpers/postLogin.js
+++ src/helpers/postLogin.js
@@ -1,7 +1,23 @@
 const crypto = require('crypto');
 
-const postLogin = (body) => {
-  console.log(body);
+const validateLogin = ({ email, password }) => {
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!email) throw new Error('O campo "email" é obrigatório');
+
+  if (!regexEmail.test(email)) {
+    throw new Error('O "email" deve ter o formato "email@email.com"');
+  }
+
+  if (!password) throw new Error('O campo "password" é obrigatório');
+
+  if (password.length < 6) {
+    throw new Error('O "password" deve ter pelo menos 6 caracteres');
+  }
+};
+
+const postLogin = (login) => {
+  validateLogin(login);
   const token = crypto.randomBytes(8).toString('hex');
   return token;
 };
