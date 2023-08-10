0fa3cf43ceaa6a252afd191033fd1f746e3bc7a6
Enzo Gennari <enzo.gennari02@gmail.com>
2023-08-10 20:30:40 -0300

    Altera nome de variável de ambiente na conexão com o banco

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'db',
+  host: process.env.MYSQL_HOSTNAME || 'db',
   port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'TalkerDB',
+  database: process.env.MYSQL_DATABASE || 'TalkerDB',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
